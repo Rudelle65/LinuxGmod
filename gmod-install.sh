c8bcfb3dd619a8f366f20163f98f991c678cdcae
Rudelle65 <39817888+Rudelle65@users.noreply.github.com>
2018-06-12 20:25:52 +0200

    Version 1.0

--- gmod-install.sh
+++ gmod-install.sh
@@ -8,26 +8,26 @@ sudo apt-get -y update
 sudo apt-get -y install screen lib32gcc1 wget curl ca-certificates
 
 wget https://raw.githubusercontent.com/Rudelle65/LinuxGmod/master/gmod-server
-chmod +x gmod-server.sh
+chmod -R +x gmod-server.sh
 
 mkdir steamcmd
 curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
 tar -xzvf steamcmd.tar.gz -C steamcmd
-chmod +x steamcmd
+chmod -R +x steamcmd
 
 mkdir serverfiles
-chmod +x serverfiles
+chmod -R +x serverfiles
 cd steamcmd
 bash ./steamcmd.sh +login anonymous +force_install_dir ../serverfiles +app_update 4020 validate +quit
 cd ..
 
 mkdir /.steam
 mkdir /.steam/sdk32
-chmod +x .steam
+chmod -R +x .steam
 cp -v linux32/steamclient.so ../.steam/sdk32/steamclient.so
 
 mkdir config
-chmod +x config
+chmod -R +x config
 cd config
 wget https://raw.githubusercontent.com/Rudelle65/LinuxGmod/master/server.cfg
 cd ..
